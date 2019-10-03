a12c24dc3f4708b2784848e6b2fb2a2aff0f9bf7
13_chengjinpeng <cjp1036934016@163.com>
2019-10-03 18:23:06 +0800

    通过spring boot来启动dubbo。 1、导入依赖 2、添加注解 3、yml配置文件修改，主要服务器中防火墙端口的设置，否则总是报错。

---
+++ boot-order-service-consumer/src/main/java/boot/com/nerl/gmall/controller/getOrderController.java
@@ -0,0 +1,35 @@
+package boot.com.nerl.gmall.controller;
+
+import com.nerl.gmall.bean.UserAddress;
+import boot.com.nerl.gmall.impl.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+/**
+ * @program: dubbo-study
+ * @description: 获取订单
+ * @author: 程金鹏
+ * @create: 2019-10-03 17:01
+ */
+@Controller
+public class getOrderController {
+
+    @Autowired
+    OrderServiceImpl orderService;
+
+    @RequestMapping("/getorder")
+    @ResponseBody
+    public List<UserAddress> getOrder() throws InterruptedException {
+        int i=0;
+        while (i<1000000){
+             orderService.initOrder("1");
+             i++;
+        }
+        return orderService.initOrder("1");
+
+    }
+}
---
+++ boot-user-service-provider/src/main/java/boot/com/nerl/gmall/impl/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package boot.com.nerl.gmall.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.nerl.gmall.bean.UserAddress;
+import com.nerl.gmall.service.UserService;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+import java.util.List;
+@Service // dubbo暴露服务
+@Component
+public class UserServiceImpl implements UserService {
+
+
+	public List<UserAddress> getUserAddressList(String userId) {
+		System.out.println("UserServiceImpl.....old...");
+		// TODO Auto-generated method stub
+		UserAddress address1 = new UserAddress(1, "北京市昌平区宏福科技园综合楼3层", "1", "李老师", "010-56253825", "Y");
+		UserAddress address2 = new UserAddress(2, "深圳市宝安区西部硅谷大厦B座3层（深圳分校）", "1", "王老师", "010-56253825", "N");
+		/*try {
+			Thread.sleep(4000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}*/
+		return Arrays.asList(address1,address2);
+	}
+
+}
